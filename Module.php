0b89654e091d5a2488897d4904bb7839158be8f8
bugadani <bugadani@bugadani-inspiron>
2012-09-09 13:05:17 +0200

    Add missing LICENSE and Module files

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
+ *
+ * For licensing information see the LICENSE file.
+ */
+
+namespace Modules\Processor;
+
+use Miny\Application\Application;
+
+class Module extends \Miny\Application\Module
+{
+    public function init(Application $app)
+    {
+        $app->add('processor', __NAMESPACE__ . '\ProcessorFactory')
+                ->addProperty('transform', __NAMESPACE__ . '\Workers\Transform')
+                ->addProperty('branch', __NAMESPACE__ . '\Workers\Branch')
+                ->addProperty('filter', __NAMESPACE__ . '\Workers\Filter');
+    }
+
+}
